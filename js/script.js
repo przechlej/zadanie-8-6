b70085a6ce1ecb5a3121e91dc887a1958a54a741
Piotr Przechlewski <haesor@gmail.com>
2018-01-28 21:04:36 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ var b = 5;
 var perimeter1 = a * a;
 var perimeter2 = 2 * a * b;
 var perimeter3 = b * b;
-var value = Math.sqrt(perimeter1) - (perimeter2) + (perimeter3)
+var value = (perimeter1) - (perimeter2) + (perimeter3);
 console.log(value);
 
 if (value > 0) {
-wynik jest dodatni
+    wynik jest dodatni;
 } else if (value < 0) {
-wynik jest ujemny}
-)
+    wynik jest ujemny;}
