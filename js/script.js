b81bf0cb0e1fc338b71dce42c1463987bb2a78b2
Piotr Przechlewski <haesor@gmail.com>
2018-01-29 19:49:04 +0100

    update

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ var perimeter1 = a * a;
 var perimeter2 = 2 * a * b;
 var perimeter3 = b * b;
 var value = (perimeter1) - (perimeter2) + (perimeter3);
-console.log(value);
-
-if (value > 0) {
-    wynik jest dodatni;
-} else if (value < 0) {
-    wynik jest ujemny;}
+if (value > 0);
+{
+    console.log("wynik jest dodatni");
+}
+else
+{
+    console.log("wynik jest ujemny");
+}
